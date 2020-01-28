6912218ec1ed0e0df7f7bacff5e40e7e3844e257
github username <github email address>
2020-01-27 19:39:30 -0800

    Refractoring
    refractor song and artist class

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,15 +20,7 @@ class Artist
   def self.all
     @@artists
   end
-=begin
-  def self.reset_all
-    self.all.clear #can clear all instances of an artist
-  end
 
-  def self.count
-    self.all.count #keeps count of all artist
-  end
-=end
   def add_song(song)
     @songs << song #adding songs to the array of songs
     song.artist = self # connecting songs to an artist
@@ -38,7 +30,5 @@ class Artist
     songs.each { |song| add_song(song) } #adding songs 
   end
 
-  #def to_param
-    #name.downcase.gsub(' ', '-')
-  #end
+  
 end
