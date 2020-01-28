4111792165c6f31fb2148649fcd3d073680f4ed0
Austin Johnson <technicallyaustin@gmail.com>
2020-01-28 01:38:37 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,8 @@
 require 'pry'
-
+## create a module of class methods and Instance methods
+## extend/include those methods in the artist and song classes
+## leave the initialize and attr_accessor
+## 
 class Artist
   attr_accessor :name
   attr_reader :songs
